db611f76eb82c06c72764372cbadc83d46bf90ee
radekp00 <radekp00@gmail.com>
2024-03-13 13:14:08 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String name = "Hello World";
+
+    for (int i = 0; i < name.length(); i++) {
+      System.out.println(name.charAt(i));
+    }
+  }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
+}
